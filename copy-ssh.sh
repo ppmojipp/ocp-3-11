94ce3c9ae08ea86f2cab185f34a1973a9564c4b3
ppmojipp <nattapong.p@dcs.premier.co.th>
2019-09-03 12:30:38 +0700

    Update copy-ssh.sh

--- copy-ssh.sh
+++ copy-ssh.sh
@@ -2,3 +2,5 @@
 #Run ssh-keygen First!!!!
 host=$1
 ssh-copy-id -i ~/.ssh/id_rsa.pub $host;
+# If ansible still connect with wrong key Please run command
+# ansible all -m ping --ask-pass --ask-sudo-pass
